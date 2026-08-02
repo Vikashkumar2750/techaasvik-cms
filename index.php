9828e10f6457274e795059f8b1fa594a1de3a86c
TechAasvik <hello@techaasvik.com>
2026-08-02 23:24:51 +0530

    Enable display_errors in production temporarily to debug 500 error

--- index.php
+++ index.php
@@ -20,8 +20,8 @@ if (APP_ENV === 'development') {
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 } else {
-    error_reporting(0);
-    ini_set('display_errors', 0);
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
     ini_set('log_errors', 1);
     ini_set('error_log', STORAGE_PATH . '/logs/php_errors.log');
 }
